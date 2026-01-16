1ad0f50083aaca7b56ac890b4012c99ce73948b6
Omkar Ankush Kashid <omkarkashidok@gmail.com>
2026-01-16 16:34:40 +0530

    Auto push 16-01-2026 16.34.40.63

--- GLTF_C_PROGRAMING/V2_gltfloding_Vertext_Data.c
+++ GLTF_C_PROGRAMING/V2_gltfloding_Vertext_Data.c
@@ -7,10 +7,36 @@
 // Read entire text file
 char* read_text_file(const char* filename)
 {
-    FILE* f = fopen(filename, "rb")
+    FILE* f = fopen(filename, "rb");
     if(!f)
     {
         return NULL;
     }
-    
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    rewind(f);
+
+    char* data = (char*)malloc(size + 1);
+    fread(data, 1, size, f);
+    data[size] = '\0';
+
+    fclose(f);
+
+    return data;
+}
+
+// Read entire binary file
+uint8_t* read_binary_file(const char* filename, size_t* outSize)
+{
+    FILE* f = fopen(filename, "rb");
+    if(!f)
+    {
+        return NULL;
+    }
+
+    fseek(f, 0, SEEK_END);
+    *outSize = ftell(f);
+    rewind(f);
+    uint8_t* data = (uint8_t*)malloc(*outSize);
+    fread(data, 1, *outSize)
 }
