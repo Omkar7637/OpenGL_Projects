39f45870149828a070c12bf171f257b025b779de
Omkar Ankush Kashid <omkarkashidok@gmail.com>
2026-01-16 11:14:40 +0530

    Auto push 16-01-2026 11.14.40.66

--- WebGL/07-PerspectiveRectangle/01-WhiteRectangle/Canvas.js
+++ WebGL/07-PerspectiveRectangle/01-WhiteRectangle/Canvas.js
@@ -1,248 +1,226 @@
 var canvas = null;
 var gl = null;
-var bFullscreen = false;
+var bFullScreen = false;
+
 var canvas_original_width;
 var canvas_original_height;
 
-// WebGL related variables
-const vertexAttributeEnum =
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame
+                            || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+var uColorUniform;
+
+// Webgl related Variables
+const MyAttributes =
 {
-    AMC_ATTRIBUTE_POSITION:0
+    AMC_ATTRIBUTE_POSITION:0,
 };
 
 var shaderProgramObject = null;
-
+var mvpUniform;
 var vao = null;
 var vbo = null;
-var mvpMatrixUniform;
+var prespectiveProjectionMatrix;
 
-var perspectiveProjectionMatrix;
-
-var requestAnimationFrame =
-    window.requestAnimationFrame ||         // google
-    window.webkitRequestAnimationFrame ||   // safari
-    window.mozRequestAnimationFrame ||      // mozilla
-    window.oRequestAnimationFrame ||        // operra
-    window.msRequestAnimationFrame;         // edge
-
-
-// Our main function
-function main()
+function main() 
 {
-    // Get Canvas aplya engine kdun magnar jo tayar kely HTML mdhe jo engine aplyala milvun denar ahe js file mdhe 
-    canvas = document.getElementById("SAG");
-    if(canvas == null)
-        console.log("Getting Canvas Failed !!!\n");
+    // Get the canvas
+    canvas = document.getElementById('AMC');
+    if (canvas == null) 
+    {
+        console.log('canvas element cannot be obtained\n');
+        return;
+    }
     else
-        console.log("Getting Canvas Succeeded !!!\n");
+    {
+        console.log('canvas element obtained successfully\n');
+    }
 
-    // set canvas width and height for future use
+
     canvas_original_width = canvas.width;
     canvas_original_height = canvas.height;
 
-     // Set Canvas background color to black
-    //context.fillStyle = "black";    
-
-    // Paint the canvas with above color
-    //context.fillRect(0, 0, canvas.width, canvas.height);
-
-    // Draw Text
-    //drawText("Hello World !!!");
-
-    // Register for keyboard events
-    window.addEventListener("keydown", keyDown, false); 
-
-    // Register for mouse events
-    window.addEventListener("click", mouseDown, false);
-
+    window.addEventListener('keydown', keyDown, false);
+    window.addEventListener('click', mouseDown, false);
     window.addEventListener("resize", resize, false);
 
+    // intialize
     initialize();
-
     resize();
-
     display();
 
-
 }
+
 
 function keyDown(event)
 {
     // Code
+
     switch(event.keyCode)
     {
-        case 81:    // Q
-        case 113:   // q
+        case 70: //F in ASCII
+        case 102: //f in ASCII
+            toggleFullScreen();
+            break;
+        case 27:
             uninitialize();
             window.close();
             break;
-
-        case 70:
-        case 102:
-            toggleFullscreen();
-            //drawText("Hello World !!!");
-            break;
+        
     }
 }
 
-function mouseDown()
+function mouseDown(event)
 {
-   
+    // Code
 }
-function toggleFullscreen()
-{
-    var fullscreen_element = 
-        document.fullscreenElement || 
-        document.webkitFullscreenElement ||
-        document.mozFullScreenElement ||
-        document.msFullscreenElement ||
-        null;
 
-    // If not fullscreen
+
+function toggleFullScreen() {
+
+    var fullscreen_element =
+    document.fullscreenElement ||
+    document.mozFullScreenElement ||
+    document.webkitFullscreenElement ||
+    document.msFullscreenElement ||
+    null;
+
     if(fullscreen_element == null)
     {
-        if(canvas.requestFullscreen)
-        {
-            canvas.requestFullscreen();
-        }
-        else if(canvas.webkitRequestFullscreen)
-        {
-            canvas.webkitRequestFullscreen();
-        }
-        else if(canvas.mozRequestFullScreen)
-        {
-            canvas.mozRequestFullScreen();
-        }
-        else if(canvas.msRequestFullscreen)
-        {
-            canvas.msRequestFullscreen();
-        }
-        bFullscreen = true;
+        if(canvas.requestFullscreen) canvas.requestFullscreen();
+        else if(canvas.mozRequestFullScreen) canvas.mozRequestFullScreen();
+        else if(canvas.webkitRequestFullscreen) canvas.webkitRequestFullscreen();
+        else if(canvas.msRequestFullscreen) canvas.msRequestFullscreen();
+
+        bFullScreen = true;
     }
-    else    // if already fullscreen
+    else
     {
-        if(document.exitFullscreen)
-        {
-            document.exitFullscreen();
-        }
-        else if(document.webkitExitFullscreen)
-        {
-            document.webkitExitFullscreen();
-        }
-        else if(document.mozCancelFullScreen)
-        {
-            document.mozCancelFullScreen();
-        }
-        else if(document.msExitFullscreen)
-        {
-            document.msExitFullscreen();
-        }
-        bFullscreen = false;
+        if(document.exitFullscreen) document.exitFullscreen();
+        else if(document.mozCancelFullScreen) document.mozCancelFullScreen();
+        else if(document.webkitExitFullscreen) document.webkitExitFullscreen();
+        else if(document.msExitFullscreen) document.msExitFullscreen();
+
+
+        bFullScreen = false;
     }
 }
 
-function initialize() {
-    // Code
-    // Get Context from above canvas
-    gl = canvas.getContext("webgl2");
-    if (gl == null)
-        console.log("Getting WebGL2-Context failed!!!\n");
-    else
-        console.log("Getting WebGL2-Context succeeded !!!\n");
 
-    // Set WebGL2 context's view width & view height properties
+function initialize()
+{
+    // Code
+    // get the 2D context from the canvas
+    gl = canvas.getContext("webgl2");
+    if (gl == null) 
+    {
+        console.log('webgl2 context cannot be obtained\n');
+        return;
+    }
+    else
+    {
+        console.log('webgl2 context obtained successfully\n');
+    }
+
+    // Set viewport width/height
     gl.viewportWidth = canvas.width;
     gl.viewportHeight = canvas.height;
 
-    // Vertex Shader
-    var vertexShaderSourceCode =
-        (
-            "#version 300 es" +
-            "\n" +
-            "in vec4 aPosition;" +
-            "uniform mat4 uMVPMatrix;" +
-            "void main(void)" +
-            "{" +
-            "gl_Position= uMVPMatrix*aPosition;" +
-            "}"
-        );
-
+    
+    // ================== Vertex Shader ==========================
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+    var vertexShaderSourceCode =
+    "#version 300 es\n" +
+    "precision mediump float;\n" +
+    "in vec4 aPosition;\n" +
+    "uniform mat4 uMVPMatrix;\n" +
+    "void main(void)\n" +
+    "{\n" +
+    "    gl_Position = uMVPMatrix * aPosition;\n" +
+    "}\n";
+
 
     gl.shaderSource(vertexShaderObject, vertexShaderSourceCode);
-
     gl.compileShader(vertexShaderObject);
 
-    if (gl.getShaderParameter(vertexShaderObject, gl.COMPILE_STATUS) == false) {
+    if (gl.getShaderParameter(vertexShaderObject, gl.COMPILE_STATUS) == false) 
+    {
         var error = gl.getShaderInfoLog(vertexShaderObject);
-        if (error.length > 0) {
-            var log = "Vertex Shader Compilation Error : " + error;
-            alert(log);
+        if(error.length > 0)
+        {
+            alert(error);
             uninitialize();
+        }else
+        {
+            console.log("Vertex shader succesfull");
         }
     }
-    else {
-        console.log("Vertex Shader Compiled Successfully !!!\n");
-    }
 
-    // Fragment shader
-    var fragmentShaderSourceCode =
-        (
-            "#version 300 es" +
-            "\n" +
-            "precision highp float;" +
-            "out vec4 FragColor;" +
-            "void main(void)" +
-            "{" +
-            "FragColor=vec4(1.0f, 1.0f, 1.0f, 1.0f);" +
-            "}"
-        );
-
+    // Fragment Shader
     var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
 
-    gl.shaderSource(fragmentShaderObject, fragmentShaderSourceCode);
+    var fragmentShaderSourceCode =
+    "#version 300 es\n" +
+    "precision mediump float;\n" +
+    "out vec4 fragColor;\n" +
+    "void main(void)\n" +
+    "{\n" +
+    "    fragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n" +
+    "}\n";
 
+
+
+
+    gl.shaderSource(fragmentShaderObject, fragmentShaderSourceCode);
     gl.compileShader(fragmentShaderObject);
 
-    if (gl.getShaderParameter(fragmentShaderObject, gl.COMPILE_STATUS) == false) {
+    if (gl.getShaderParameter(fragmentShaderObject, gl.COMPILE_STATUS) == false) 
+    {
         var error = gl.getShaderInfoLog(fragmentShaderObject);
-        if (error.length > 0) {
-            var log = "Fragment Shader Compilation Error  : " + error;
-            alert(log);
+        if(error.length > 0)
+        {
+            alert(error);
             uninitialize();
+        } 
+        else 
+        {
+            console.log("Fragment shader successful");
         }
     }
-    else {
-        console.log("Fragment Shader Compiled Successfully !!!\n");
-    }
 
-    // Shader Program
     shaderProgramObject = gl.createProgram();
 
     gl.attachShader(shaderProgramObject, vertexShaderObject);
     gl.attachShader(shaderProgramObject, fragmentShaderObject);
 
-    // pre-linking binding
-    gl.bindAttribLocation(shaderProgramObject, vertexAttributeEnum.AMC_ATTRIBUTE_POSITION, "aPosition");
+    // Attribute binding
+    gl.bindAttribLocation(shaderProgramObject, MyAttributes.AMC_ATTRIBUTE_POSITION, "aPosition");
 
-    // Linking
+    // Link
     gl.linkProgram(shaderProgramObject);
 
-    if (gl.getProgramParameter(shaderProgramObject, gl.LINK_STATUS) == false) {
+    if(gl.getProgramParameter(shaderProgramObject, gl.LINK_STATUS) == false) 
+    {
         var error = gl.getProgramInfoLog(shaderProgramObject);
-        if (error.length > 0) {
-            var log = "Shader Program Linking Error : " + error;
-            alert(log);
+        if(error.length > 0)
+        {
+            alert(error);
             uninitialize();
+        } 
+        else 
+        {
+            console.log("Shader Program linked successfully");
         }
     }
-    else {
-        console.log("Shader Program Linked Successfully !!!\n");
+    else 
+    {
+        console.log("Shader Program linked successfully");
     }
 
-    mvpMatrixUniform = gl.getUniformLocation(shaderProgramObject, "uMVPMatrix");
+    // get uniform location 
+    uMVPMatrixUniform = gl.getUniformLocation(shaderProgramObject, "uMVPMatrix");
+    uColorUniform = gl.getUniformLocation(shaderProgramObject, "uColor");   // NEW
 
-    // Geomatry attribute array declaration
     var rectangle_position = new Float32Array([
         -1.0, -1.0, 0.0,
         -1.0, 1.0, 0.0,
@@ -250,72 +228,64 @@ function initialize() {
         1.0, -1.0, 0.0
     ]);
 
-    // Vao
+
     vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
 
-    // VBO
+    // Position VBO
     vbo = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.bufferData(gl.ARRAY_BUFFER, rectangle_position, gl.STATIC_DRAW);
-    gl.vertexAttribPointer(vertexAttributeEnum.AMC_ATTRIBUTE_POSITION, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vertexAttributeEnum.AMC_ATTRIBUTE_POSITION);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.vertexAttribPointer(MyAttributes.AMC_ATTRIBUTE_POSITION, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(MyAttributes.AMC_ATTRIBUTE_POSITION);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
     gl.bindVertexArray(null);
 
-    // Depth Initialization
+    // enable depth
     gl.clearDepth(1.0);
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
 
+    prespectiveProjectionMatrix = mat4.create();
 
-    // Set clear color
-    // 1st WebGL API
+    // Clear color
     gl.clearColor(0.0, 0.0, 1.0, 1.0);
-
-    // initialize projection matrix
-    // first time we are using func of min.js
-    perspectiveProjectionMatrix = mat4.create();
-
 }
 
 function resize()
 {
     // Code
-    if (canvas.height <= 0) {
-        canvas.height = 1;
-    }
-    if (bFullscreen == true) {
+    if(bFullScreen == true)
+    {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
     }
-    else {
+    else
+    {
         canvas.width = canvas_original_width;
         canvas.height = canvas_original_height;
     }
-
-    // Set viewport
     gl.viewport(0, 0, canvas.width, canvas.height);
 
-    // Set Perspective Projection
-    mat4.perspective(perspectiveProjectionMatrix, 45.0, parseFloat(canvas.width) / parseFloat(canvas.height), 0.1, 100.0)
+    // set prespective projection
+    mat4.perspective(prespectiveProjectionMatrix, 45.0, parseFloat(canvas.width)/parseFloat(canvas.height), 0.1, 100.0);
 }
-
 function display()
 {
-    // Code
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.useProgram(shaderProgramObject);
 
-    // Transformations
+
+    // ModelView Matrix (Translate)
     var modelViewMatrix = mat4.create();
+    mat4.translate(modelViewMatrix, modelViewMatrix, [0.0, 0.0, -4.0]);
+
+    // Compute MVP
     var modelViewProjectionMatrix = mat4.create();
+    mat4.multiply(modelViewProjectionMatrix, prespectiveProjectionMatrix, modelViewMatrix);
 
-    mat4.translate(modelViewMatrix, modelViewMatrix, [0.0, 0.0, -3.0]);
-    mat4.multiply(modelViewProjectionMatrix, perspectiveProjectionMatrix, modelViewMatrix);
-
-    gl.uniformMatrix4fv(mvpMatrixUniform, false, modelViewProjectionMatrix);
+    gl.uniformMatrix4fv(uMVPMatrixUniform, false, modelViewProjectionMatrix);
 
     gl.bindVertexArray(vao);
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
@@ -323,47 +293,59 @@ function display()
 
     gl.useProgram(null);
 
-    update();
-
-    // do Dubble buffering
     requestAnimationFrame(display, canvas);
 }
 
+
+
 function update()
 {
-    //Code
+    // code
 }
 
 function uninitialize()
 {
-    // Code
+
+    // Exit fullscreen if needed
+    if(bFullScreen)
+    {
+        toggleFullScreen();
+    }
+
+    if(vbo)
+    {
+        gl.deleteBuffer(vbo);
+        vbo = null;
+    }
+
+    if(vao)
+    {
+        gl.deleteVertexArray(vao);
+        vao = null;
+    }
+
     
-    if (shaderProgramObject)    // this stmt can be if(shaderProgramObject != null)
+
+
+    if(shaderProgramObject)
     {
         gl.useProgram(shaderProgramObject);
+
         var shaderObjects = gl.getAttachedShaders(shaderProgramObject);
-        if (shaderObjects && shaderObjects.length > 0) {
-            for (let i = 0; i < shaderObjects.length; i++) {
-                gl.detachShader(shaderProgramObject, shaderObjects[i]);
-                gl.deleteShader(shaderObjects[i]);
-                shaderObjects[i] = null;
-            }
+
+        for(let i = 0; i < shaderObjects.length; i++)
+        {
+            gl.detachShader(shaderProgramObject, shaderObjects[i]);
+            gl.deleteShader(shaderObjects[i]);
+            shaderObjects[i] = null;
         }
+
         gl.useProgram(null);
         gl.deleteProgram(shaderProgramObject);
         shaderProgramObject = null;
     }
 
-    if (vbo) {
-        gl.deleteBuffer(vbo);
-        vbo = null;
-    }
-    if (vao) {
-        gl.deleteVertexArray(vao);
-        vao = null;
-    }
-   // window.close();
+
+
 }
-
-
 
