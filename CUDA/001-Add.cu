e4450b70701e3bebbd961b86439097dee13d183d
Omkar Ankush Kashid <omkarkashidok@gmail.com>
2026-02-16 12:34:40 +0530

    Auto push 16-02-2026 12.34.40.82

---
+++ CUDA/001-Add.cu
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<cuda_runtime.h>
+
+#define N 10
+
+// GPU Kernal (runs on GPU)
+__global__ void vectorAdd(int *A, int *B, int *C)
+{
+    int i = threadIdx.x; // thread ID
+    C[i] = A[i] + B[i];
+}
+
+int main()
+{
+    int h_A[N], h_B[N], h_C[N]; // Host (CPU) arrays
+
+    // intialize arrya
+    for(int i = 0; i < N; i++)
+    {
+        h_A[i] = i;
+        h_B[i] = i * 10;
+    }
+
+    int *d_A, *d_B, *d_C; // 
+}
