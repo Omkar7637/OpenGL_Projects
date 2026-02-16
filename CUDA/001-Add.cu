3c248cd41b150f718bae448f49777c8b66792601
Omkar Ankush Kashid <omkarkashidok@gmail.com>
2026-02-16 12:39:40 +0530

    Auto push 16-02-2026 12.39.40.76

--- CUDA/001-Add.cu
+++ CUDA/001-Add.cu
@@ -21,5 +21,16 @@ int main()
         h_B[i] = i * 10;
     }
 
-    int *d_A, *d_B, *d_C; // 
+    int *d_A, *d_B, *d_C; // Device (GPU) pointers
+
+    // 1) allocate GPU memory
+    cudaMalloc((void**)&d_A, N*sizeof(int));
+    cudaMalloc((void**)&d_B, N*sizeof(int));
+    cudaMalloc((void**)&d_C, N*sizeof(int));
+
+    //2) cpoy CPU -> GPU
+    cudaMemcpy(d_A, h_A, )
+
+    
+
 }
